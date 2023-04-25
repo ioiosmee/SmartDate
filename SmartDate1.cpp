98f960a97cedb550ef60a63182d71a49286ddcd6
Yasmina <bakhhtir@gmail.com>
2023-04-25 21:00:09 +0300

    Defined class SmartDate

--- SmartDate1.cpp
+++ SmartDate1.cpp
@@ -1,12 +1,29 @@
 ﻿//проект "kласс умная дата"
 
 #include <iostream>
-/*
+
 using std::cout;
 using std::cin;
 using std::endl;
-*/
+
+class SmartDate
+{
+private:
+    short day;
+    short month;
+    short year;
+public:
+    SmartDate(short d, short m, short y);
+};
+SmartDate::SmartDate(short d, short m, short y)
+{
+    day = d;
+    month = m;
+    year = y;
+}
 int main()
 {
-    std::cout << "Hello World!\n";
+    SmartDate d1(25, 04, 2023);
 }
+
+
